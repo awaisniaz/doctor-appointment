2d740221699ccb3a474a5062168ad8f6ef74961b
Awais Niaz <awais.niaz@mwanmobile.org>
2022-02-21 21:09:45 +0500

    header design

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 
 import './App.css';
-import logo from './logo.png'
+import Header from './Components/Header';
 function App() {
-  return (<div className="columns-2 p-3 border-slate-100">
-    <div>
-      <img src={logo} className='h-14 pr-10 ' alt='logo Image'></img>
-    </div>
-    <div className='flex justify-end'>
-      <button className='bg-cyan-400 p-3 rounded text-gray-50 font-bold hover:bg-cyan-410'>Login/Signup</button>
-    </div>
-  </div>
+  return (
+    <Header/>
   );
 }
 
