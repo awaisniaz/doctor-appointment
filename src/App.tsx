52f9655a7b1bf7d8ffc54ccc072006897744f8ad
Awais Niaz <awais.niaz@mwanmobile.org>
2022-02-21 18:04:46 +0500

    Header design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import logo from './logo.png'
 function App() {
-  return (<p>Doctor Booking app</p>
+  return (<div className="columns-2 p-3 border-slate-100">
+    <div>
+      <img src={logo} className='h-14 pr-10 ' alt='logo Image'></img>
+    </div>
+    <div className='flex justify-end'>
+      <button className='bg-cyan-400 p-3 rounded text-gray-50 font-bold hover:bg-cyan-410'>Login/Signup</button>
+    </div>
+  </div>
   );
 }
 
