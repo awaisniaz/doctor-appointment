b910393e2cc2baa2e0564c7a49ce45751ea1b1d2
Awais Niaz <awais.niaz@mwanmobile.org>
2022-02-25 18:31:57 +0500

    Some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 
 import './App.css';
-import Header from './Components/Header';
-import Banner from './Components/banner';
 function App() {
   return (
     <>
-      <Header />
-      <Banner />
     </>
   );
 }
