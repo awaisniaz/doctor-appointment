04a789d4c0a44356e5d2543f5a42ba4ad1f3acf9
Awais Niaz <awais.niaz@mwanmobile.org>
2022-03-05 20:16:32 +0500

    some design changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './App.css';
 import Header from './Components/Header';
+import Banner from './Components/Banner';
+import LookingFor from './Components/LookingFor';
 function App() {
   return (
+    <>
     <Header></Header>
+    <Banner/>
+    <LookingFor/>
+    </>
   );
 }
 
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,36 @@
 import * as React from 'react';
-const Header: React.FunctionComponent = ({}) => {
-  return (<div className='container'
-  ></div>);
+import './style.css'
+import { menudata } from '../../config/menu_data';
+import {Typography,Button } from 'antd';
+import { DownOutlined,GlobalOutlined } from '@ant-design/icons'
+
+const Header: React.FunctionComponent = ({ }) => {
+  const { Title,Text } = Typography
+  return (<div className='container-header'>
+    <div className='left-section'>
+      <img src={'./images/logo.png'}></img>
+      <div className='menu-option'>
+        {
+          menudata?.map(item => {
+            return <Title level={5}>{item?.title} {item?.active ? <DownOutlined className='more-menu' /> : null}</Title>
+          })
+        }
+      </div>
+    </div>
+    <div className='right-section'>
+
+    <div className='contact-section'>
+      <div className='contact-logo'>
+      <GlobalOutlined />
+      </div>
+      <div className='contacte-details'>
+        <Text type='secondary'>Contact</Text>
+        <Text className=''><b>+92362166162616</b></Text>
+      </div>
+    </div>
+    <Button className='login-btn'>LOGIN / SIGNUP</Button>
+    </div>
+  </div>);
 };
 
 export default Header;
