5b94a27c76bef84f8eb5fd806a338050e8103fc7
Muhammad Awais Niaz <awaisniaz1995@gmail.com>
2022-03-23 11:47:07 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +9,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
 reportWebVitals();
